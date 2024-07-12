18ad92e4424f789e846c93791f7609711da7a28d
Noahwealsey <harshgaur563@gmail.com>
2024-07-12 10:47:39 +0530

    second mobius deployed with memoisation

--- mobius.cpp
+++ mobius.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
+#include <unordered_map>
+
+std::unordered_map<int, int> memo;
 
 int mobius(int n){
-	if(n ==1){
-		return 0;
-	}
-	
+
+	if(memo.find(n) != memo.end()) return memo[n];
+
+  	int orig_val = n;	
 	int pf = 0;
 
 	for(int i =0; i*i >= n; i++){
 		if(n%i == 0){
-			if((n/i)%i == 0) return 0;
-				pf++;
-				n /= i;	
-			}
+			if((n/i)%i == 0){
+				memo[origin_val] = 0;
+				return 0;	
+			} 
+			pf++;
+			n /= i;		
 		}
  	 } 
 	
 	if(n > 1) pf++;
 
-	return(pf % 2 == 0) ? 1: -1;
+	int result = (pf % 2 == 0) ? 1: -1;
+	memo[orig_val] = result;
+	return result;
 }
 
 int main(){
